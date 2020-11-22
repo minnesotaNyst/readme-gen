0ae2117a9797c7b58484cd1c81862c14660328a7
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-11-22 16:48:05 -0600

    successfully generating a readme file

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 const inquirer = require('inquirer');
-
-/* const generateMarkdown = require('./utils/generateMarkdown.js'); */
+const fs = require('fs');
+const generateMarkdown = require('./src/generateMarkdown.js');
 
 // array of questions for user
-const questions = () => {
+const promptUser = () => {
 	return inquirer.prompt([
 		{
 			type: 'input',
 			name: 'name',
-			message: 'What is the name of your project? (Requried)',
+			message: 'What is your name? (Required)',
 			validate: nameInput => {
 				if (nameInput) {
+					return true;
+				} else {
+					console.log('Please enter your name!');
+					return false;
+				}
+			}
+		},
+		{
+			type: 'confirm',
+			name: 'confirmAbout',
+			message:
+				'Would you like to enter some information about yourself for an "About" section?',
+			default: true
+		},
+		{
+			type: 'input',
+			name: 'about',
+			message: 'Provide some information about yourself:',
+			when: ({ confirmAbout }) => {
+				if (confirmAbout) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+	]);
+};
+
+const questions = data => {
+	if (!data.info) {
+		data.info = [];
+	}
+	console.log(`
+	===============
+	README MARKDOWN
+	===============
+	`);
+	return inquirer.prompt([
+		{
+			type: 'input',
+			name: 'title',
+			message: 'What is the title of your project? (Requried)',
+			validate: title => {
+				if (title) {
 					return true;
 				} else {
 					console.log('Please enter a project name!');
@@ -30,15 +75,44 @@ const questions = () => {
 					return false;
 				}
 			}
+		},
+		{
+			type: 'input',
+			name: 'reason',
+			message: 'What prompted you to create this application?',
+			validate: reason => {
+				if (reason) {
+					return true;
+				} else {
+					console.log('Please provide a reason!');
+					return false;
+				}
+			}
 		}
 	]);
 };
 
 // function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {}
+//function init() {}
 
 // function call to initialize program
-init();
+//init();
+
+/* const pageMD = generateMarkdown(data); */
+
+promptUser()
+	.then(questions)
+	.then(data => {
+		const pageMD = generateMarkdown(data);
+
+		fs.writeFile('./g-readme.md', pageMD, err => {
+			if (err) throw new Error(err);
+
+			console.log(
+				'Your README is complete! Check it out g-readme.md to see the output!'
+			);
+		});
+	});
