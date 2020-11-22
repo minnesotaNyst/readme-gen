c8aad9d8dde916bebb549260c9388f22f4e8f7d5
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-11-22 11:47:04 -0600

    updated package.json file

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,31 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+/* const generateMarkdown = require('./utils/generateMarkdown.js'); */
+
+// array of questions for user
+const questions = () => {
+	return inquirer.prompt([
+		{
+			type: 'input',
+			name: 'name',
+			message: 'What is your name? (Required)',
+			validate: nameInput => {
+				if (nameInput) {
+					return true;
+				} else {
+					console.log('Please enter your name!');
+					return false;
+				}
+			}
+		}
+	]);
+};
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
